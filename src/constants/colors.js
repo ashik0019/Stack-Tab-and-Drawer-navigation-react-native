3d492d3fcff5fac0a4650f6ae9477ec2d503c515
Md Ashiqur Rahman <ashiqur.rahman0019@gmail.com>
2022-09-19 02:10:32 +0600

    inital commit

---
+++ src/constants/colors.js
@@ -0,0 +1,12 @@
+export default {
+  gradientForm: '#FF4FA7',
+  primary: '#FF1D8E',
+  white: '#FFFFFF',
+  dark: '#444',
+  bgColor: '#e6e6e6',
+  warning: '#f0d500',
+  danger: '#FF0D0E',
+  gray: '#666666',
+  grayLight: '#ccc',
+  black: '#0a0a0a',
+};
